b4ead2102920beb47aa44921d801461e23574819
StavreskiB <stavreskibojan@gmail.com>
2021-11-23 01:46:44 +0100

    first commit

---
+++ src/main/java/fastfoodbackend/fastfoodbackend/FastfoodbackendApplication.java
@@ -0,0 +1,13 @@
+package fastfoodbackend.fastfoodbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastfoodbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastfoodbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fastfoodbackend/fastfoodbackend/FastfoodbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fastfoodbackend.fastfoodbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastfoodbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
